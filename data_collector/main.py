a409eb721827a78890b29aef188b3fd9af6b0649
nico-de-vietri <113310954+nico-de-vietri@users.noreply.github.com>
2025-07-25 12:04:57 +0200

    Update main.py

--- data_collector/main.py
+++ data_collector/main.py
@@ -1,1 +1,20 @@
 
+import asyncio
+import websockets
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+AIS_API_KEY = os.getenv("AIS_API_KEY")
+
+async def main():
+    uri = f"wss://aisstream.io/api/v1/stream?apikey={AIS_API_KEY}"
+
+    async with websockets.connect(uri) as websocket:
+        while True:
+            message = await websocket.recv()
+            print(message) 
+
+if __name__ == "__main__":
+    asyncio.run(main())
